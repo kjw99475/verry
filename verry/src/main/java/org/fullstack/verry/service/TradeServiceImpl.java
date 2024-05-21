079789a0ef5dfe46471db79cc8cb689272b6ad09
cmkbeew <cmkbeew@naver.com>
2024-05-22 08:57:54 +0900

    중고거래 리스트 추가

--- verry/src/main/java/org/fullstack/verry/service/TradeServiceImpl.java
+++ verry/src/main/java/org/fullstack/verry/service/TradeServiceImpl.java
@@ -3,10 +3,19 @@ package org.fullstack.verry.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.fullstack.verry.domain.TradeEntity;
+import org.fullstack.verry.dto.BoardDTO;
+import org.fullstack.verry.dto.PageRequestDTO;
+import org.fullstack.verry.dto.PageResponseDTO;
 import org.fullstack.verry.dto.TradeDTO;
 import org.fullstack.verry.repository.TradeRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Log4j2
 @Service
@@ -23,5 +32,27 @@ public class TradeServiceImpl implements TradeService {
         int idx = tradeRepository.save(trade).getTradeIdx();
 
         return idx;
+    }
+
+    @Override
+    public PageResponseDTO<TradeDTO> list(PageRequestDTO pageRequestDTO) {
+        PageRequest pageable = pageRequestDTO.getPageable();
+
+//        Page<TradeEntity> result = tradeRepository.findAll(PageRequest.of(pageRequestDTO.getPage(), pageRequestDTO.getPage_size(), Sort.by("tradeIdx").descending()));
+        Page<TradeEntity> result = tradeRepository.findAll(pageable);
+
+//        log.info("result : {}", result.getContent());
+        log.info("result : {}", result.getContent());
+
+        List<TradeDTO> dtoList = result.getContent().stream()
+                .map(trade -> modelMapper.map(trade, TradeDTO.class))
+                .collect(Collectors.toList());
+
+        return PageResponseDTO.<TradeDTO>withAll()
+                .pageRequestDTO(pageRequestDTO)
+                .dtoList(dtoList)
+                .total_count((int)result.getTotalElements())
+                .build();
+//        return null;
     }
 }
