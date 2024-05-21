7f7b6cdae3a0a9d821d6bca0d9d793234f403b01
DESKTOP-0H1N2HM\kjw <kjw99475@gmail.com>
2024-05-21 14:33:14 +0900

    etc: start

---
+++ verry/src/test/java/org/fullstack/verry/VerryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fullstack.verry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
